ab077e8d951d19bb47b67fefefc7175881d18ccd
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-18 20:00:20 +0000

    Added the input and name in application fuction

--- src/index.js
+++ src/index.js
@@ -3,32 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
+  const reset = () => {
+    console.log("reset");
+    // your code here
+  };
   // console.log(props);
-  const [state, setState] = useState("RESET");
+  //const [state, setState] = useState("RESET");
   //const {state, setState} = useState("RESET");
   //const state = 'RESET';
   //const handleClick = () => Application();
 
   
   return (
-    <button onClick={() => console.log('RESET')}>{state}</button>
+    <button onClick={reset}>RESET</button>
   );// your code here
 };
+
+
 
 const Application = (props) => {
 
   // your code here
 
-  const reset = () => {
-    console.log("reset");
-    // your code here
-  };
+
+  const [name, setName] = useState("Ari");
 
   return (
     <main>
+      <input placeholder="Type your name"></input> 
       <Button />
       {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <h1>Hello {name}</h1>
     </main>
   );
 };
